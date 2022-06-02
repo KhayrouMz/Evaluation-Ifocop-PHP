cc344853d49d1a4771c80017f5f4c1e435dd7e0b
khayreddine <khayreddine.mezerreg@gmail.com>
2022-06-02 16:52:39 +0200

    modif

--- admin/gestion_produit.php
+++ admin/gestion_produit.php
@@ -1,14 +1,78 @@
 <?php
 require_once('../include/init.php');
 
+if (!internauteConnecteAdmin()){
+    header('location:'.URL.'connexion.php');
+    exit();
+}
+
+if (isset($_GET['action'])){
+        if($_POST){
+            if(!isset($_POST['prix']) || !preg_match('#^[0-9]{1,4}$#', $_POST['prix'])){
+                $erreur .= '<div class="alert alert-danger" role="alert">Erreur format prix !</div>';
+            }
+            if (!isset($_POST['date_arrivee']) ){
+                $erreur .= '<div class="alert alert-danger" role="alert">Erreur format date d\'arrivée !</div>';
+            }
+            if (!isset($_POST['date_depart']) ){
+                $erreur .= '<div class="alert alert-danger" role="alert">Erreur format date de départ !</div>';
+            }
+            
+            if (!isset($_POST['id_salle'])){
+                $erreur .= '<div class="alert alert-danger" role="alert">Erreur vous n\'avez pas selectionné votre salle !</div>';
+            }
+            // if(!isset($_POST['etat']) || $_POST['etat'] != "libre" && $_POST['etat'] != "reservation"){
+            //     $erreur .= '<div class="alert alert-danger" role="alert">Erreur format etat !</div>';
+            // }
+            if (empty($erreur)){
+                if($_GET['action'] == 'update'){
+                    $modifierProduit = $pdo->prepare("UPDATE produit SET id_produit = :id_produit, date_arrivee = :date_arrivee, date_derat = :date_derat, id_salle = :id_salle, prix = :prix WHERE id_produit = :id_produit ");
+                    $modifierProduit->bindValue(':id_produit', $_POST['id_produit'], PDO::PARAM_INT);
+                    $modifierProduit->bindValue(':date_arrivee', $_POST['date_arrivee'], PDO::PARAM_STR);
+                    $modifierProduit->bindValue(':date_derat', $_POST['date_derat'], PDO::PARAM_STR);
+                    $modifierProduit->bindValue(':id_salle', $_POST['id_salle'], PDO::PARAM_INT);
+                    $modifierProduit->bindValue(':prix', $_POST['prix'], PDO::PARAM_INT);
+                    // $modifierProduit->bindValue(':etat', $_POST['etat'], PDO::PARAM_STR);
+                    $modifierProduit->execute();
+            }else{
+                $ajouterProduit = $pdo->prepare("INSERT INTO produit (date_arrivee, date_depart, id_salle, prix) VALUES (:date_arrivee, :date_depart, :id_salle, :prix)");
+                $ajouterProduit->bindValue(':date_arrivee', $_POST['date_arrivee'], PDO::PARAM_STR);
+                $ajouterProduit->bindValue(':date_depart', $_POST['date_depart'], PDO::PARAM_STR);
+                $ajouterProduit->bindValue(':id_salle', $_POST['id_salle'], PDO::PARAM_INT);
+                $ajouterProduit->bindValue(':prix', $_POST['prix'], PDO::PARAM_INT);
+                // $ajouterProduit->bindValue(':etat', $_POST['etat'], PDO::PARAM_STR);
+                $ajouterProduit->execute();
+            }
+        }
+    }
+    if($_GET['action'] == 'update' ){
+        $queryProduits = $pdo->query("SELECT * FROM produit WHERE id_produit = '$_GET[id_produit]' ");
+        $produitActuel = $queryProduits->fetch(PDO::FETCH_ASSOC);
+    }
+
+    $id_produit = (isset($produitActuel['id_produit'] )) ? $produitActuel['id_produit'] : "";
+    $date_arrivee = (isset($produitActuel['date_arrivee'] )) ? $produitActuel['date_arrivee'] : "";
+    $date_depart = (isset($produitActuel['date_depart'] )) ? $produitActuel['date_depart'] : "";
+    $id_salle = (isset($produitActuel['id_salle'] )) ? $produitActuel['id_salle'] : "";
+    $prix = (isset($produitActuel['prix'] )) ? $produitActuel['prix'] : "";
+    // $etat = (isset($produitActuel['etat'] )) ? $produitActuel['etat'] : "";
+
+    if($_GET['action'] == 'delete' ){
+        $pdo->query(" DELETE FROM produit WHERE id_produit = '$_GET[id_produit]'");
+    }
+
+
+}
+
 
 
 require_once('./includeAdmin/header.php');
 ?>
 
-
+<?= $erreur ?>
 
 <form method="POST">
+<input type="hidden" name="id_produit" value="<?= $id_produit ?>">
     <div class="row mt-5 offset-2">
         <div class="col-md-4">
             <label class="form-label" for="date_arrivee"><div class="badge badge-dark text-dark">Date d'arrivée</div></label>
@@ -21,17 +85,22 @@ require_once('./includeAdmin/header.php');
     </div>
     <div class="row mt-5 offset-2">
         <div class="col-md-4">
-            <label class="form-label" for="id_produit"><div class="badge badge-dark text-dark">produit</div></label>
-            <select class="form-select" name="ville" id="ville">
-                    <option value="">Choisissez la produit</option>
-                    <option class="text-dark" value=" paris">Paris</option>
-                    <option class="text-dark" value=" lyon" >Lyon</option>
-                    <option class="text-dark" value=" marseille ">Marseille</option>
-                </select>
+            <label class="form-label" for="id_salle"><div class="badge badge-dark text-dark">Salle</div></label>
+            <select class="form-select" name="id_salle" id="id_salle">
+                <?php $afficheSalles = $pdo->query("SELECT DISTINCT id_salle FROM salle ORDER BY id_salle ASC") ;?>
+                <option>Choisissez le ID de la salle</option>
+                <?php while( $salle = $afficheSalles->fetch(PDO::FETCH_ASSOC) ): ?>
+                        
+                    <?php foreach( $salle as $key => $value): ?>
+                        <option class="text-dark" value="id_salle "<?= ($value == $id_salle ) ? 'selected' : "" ?>><?= $value ?></option>
+                    <?php endforeach ; ?>
+                <?php endwhile ; ?>
+            </select>
         </div>
+        
         <div class="col-md-4">
             <label class="form-label" for="prix"><div class="badge badge-dark text-dark">Prix</div></label>
-            <input class="form-control" type="text" name="prix" value="" id="prix"  placeholder="prix">
+            <input class="form-control" type="text" name="prix" value="<?= $prix ?>" id="prix"  placeholder="Prix">
         </div>
     </div>
     <div class="col-md-1 mt-5">
@@ -39,6 +108,16 @@ require_once('./includeAdmin/header.php');
     </div>
 </form>
 
+<?php $queryproduits = $pdo->query("SELECT id_produit FROM produit") ?>
+<h2 class="py-5">Nombre de produit en base de données: <?= $queryproduits->rowCount() ?></h2>
+
+<div class="row justify-content-center py-5">
+    <a href='?action=add' class="d-flex justify-content-center align-items-center">
+        <button type="button" class="btn btn-sm btn-outline-dark shadow rounded">
+        <i class="bi bi-plus-circle-fill"></i> Ajouter un produit
+        </button>
+    </a>
+</div>
 
 <table class="table  text-center">
 <?php $affciheProduits = $pdo->query("SELECT * FROM produit ORDER BY id_produit ASC") ?>
@@ -62,13 +141,14 @@ require_once('./includeAdmin/header.php');
                     <td><?= $value ?></td>
                 <?php endif ?>
             <?php endforeach ?>
-            <td><a href='?action=update&id_produit=<?= $produit['id_produit'] ?>'><i class="bi bi-pencil-square text-white" style="font-size: 1.5rem;"></i></a></td>
+            <td><a href='?action=update&id_produit=<?= $produit['id_produit'] ?>'><i class="bi bi-pencil-square text-dark" style="font-size: 1.5rem;"></i></a></td>
             <td><a data-href="?action=delete&id_produit=<?= $produit['id_produit'] ?>" data-toggle="modal" data-target="#confirm-delete"><i class="bi bi-trash text-danger" style="font-size: 1.5rem;"></i></a></td>
         </tr>
         <?php endwhile ?>
     </tbody>
 </table>
 
+<?php echo '<pre>' ; print_r($salle) ; echo '</pre>'; ?>
 
 
 
